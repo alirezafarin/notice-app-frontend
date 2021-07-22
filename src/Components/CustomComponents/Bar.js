5b564ef5a79f2fb63425fc805c6d862dd393ec42
alirezafarin <alirafarinzad@gmail.com>
2021-07-22 23:32:51 +0430

    ui modification

--- src/Components/CustomComponents/Bar.js
+++ src/Components/CustomComponents/Bar.js
@@ -2,25 +2,34 @@ import React, { Component } from 'react'
 
 import megaphone from 'icons/megaphone.png';
 import CustomButton from 'Components/MuiComponents/CustomButton';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-class Bar extends Component {
-  render() {
-    return (
-      <div id='top-bar'>
-        <div className='bar-logo'>
-          <img src={megaphone} alt='logo' />
-        </div>
-        <div className='bar-button'>
-          <CustomButton
-            text='ورود'
-            color='primary'
-            icon='profile'
-            variant='contained'
-          />
-        </div>
+function Bar() { 
+
+  const matches = useMediaQuery('(min-width:600px)');
+
+  return(
+    <div id='top-bar'>
+      <div className='bar-logo'>
+        <img src={megaphone} alt='logo' />
       </div>
-    )
-  }
+      <div className='bar-buttons'>
+        <CustomButton
+          text='ورود'
+          color='primary'
+          icon='profile'
+          variant='contained'
+        />
+        {matches && 
+          <CustomButton
+          text='ثبت آگهی'
+          color='primary'
+          icon='add'
+          variant='contained'
+        />}
+      </div>
+    </div>
+  );
 }
 
 export default Bar;
--- src/Components/Routes/Home.js
+++ src/Components/Routes/Home.js
@@ -1,18 +1,24 @@
+import { useMediaQuery } from '@material-ui/core';
 import NoticeCard from 'Components/CustomComponents/NoticeCard'
 import CustomButton from 'Components/MuiComponents/CustomButton'
 import React from 'react'
 
 export default function Home() {
+
+  const matches = useMediaQuery('(max-width:599px)');
+
   return (
     <div id='home'>
-      <div className='home-add-notice-btn'>
-        <CustomButton
-          text='ثبت آگهی'
-          color='primary'
-          icon='add'
-          variant='contained'
-        />
-      </div>
+      {matches &&
+        <div className='home-add-notice-btn'>
+          <CustomButton
+            text='ثبت آگهی'
+            color='primary'
+            icon='add'
+            variant='contained'
+          />
+        </div> 
+      }
       <div className='home-notices'>
         <NoticeCard />
       </div>
